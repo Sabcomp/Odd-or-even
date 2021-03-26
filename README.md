de0523bff27ae591b57b9938a16379a40c33dc15
Sabcomp <alushisabrina93@gmail.com>
2021-03-26 20:56:27 +0000

    Create the final version of odd_or_even.py

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ When the user gives you the number, you check if it's odd or even and then you p
 
 Example:
 
-Prompt: What number are you thinking?
+Prompt1: What number are you thinking?
 Input: 25
-Output: That's an odd number! Have another?  
+Output: That's an odd number! 
+Prompt2: Have another?  
+Input: Y / y
+Output: Have a nice day!
+
+
--- odd_or_even.py
+++ odd_or_even.py
@@ -1,13 +1,16 @@
-# Input
-def odd_or_even():
-  num = int(input("What number are you thinking?"))
+while True:
+  num = int(input("What number are you thinking? "))
+  
+  # Test for parity
   if num % 2 == 0:
     print("That's an even number!")
   else:
-    print("That's an odd number!") 
-odd_or_even()
-question = input("Have another?")
-while question == 'Y' or question != 'y':
-  odd_or_even()
-else:
-  print("Have a good day!)
+    print("That's an odd number!")
+  
+  # Deciding whether to continue the program
+  question = input("Have another? ")
+  if question == 'N' or question == 'n':
+    break
+
+# The Greeting
+print("Have a lovely day!")
